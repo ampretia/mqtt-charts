cdcd42fe24fc4cd631cb3f74716062a9b89584d9
Matthew B White <whitemat@uk.ibm.com>
2021-02-10 09:17:41 +0000

    Prototype finally working
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,34 +3,34 @@
     <h1 class="title">Marvin Configurator</h1>
     <div class="section">
       <div class="box has-text-left">
-        <version-select
-          class="pb-6"
-          :versions="versions('chaincode', 'node')"
-          title="Node Chaincode (release14)"
-          @update="update($event, 'chaincode', 'node', 'release14')"
-        />
-
-        <version-select
-          class="pb-6"
-          :versions="versions('chaincode', 'node')"
-          title="Node Chaincode (release20)"
-          @update="update($event, 'chaincode', 'node', 'release20')"
-        />
-
-        <version-select
-          class="pb-6"
-          :versions="versions('sdk', 'node')"
-          title="Node SDK (release14)"
-          @update="update($event, 'sdk', 'node', 'release14')"
-        />
+        <h2 class="subtitle">Chaincode</h2>
+        <div
+          v-for="(versions, modules) in versions('chaincode')"
+          v-bind:key="modules"
+        >
+          For module written to <b>{{ modules }} </b>
+          <div
+            v-for="(versionchoices, modulename) in versions"
+            v-bind:key="versionchoices"
+          >
+            <version-select class="pb-3"
+              :versions="versionchoices"
+              :title="modulename"
+              @update="update($event, 'chaincode', modules, modulename)"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="box has-text-left">
+        <pre classs="has-text-left">{{ tocopy }}</pre>
       </div>
       <div class="box has-text-left">
         <VueJsonPretty v-bind:data="updatedversions" :key="componentKey">
         </VueJsonPretty>
       </div>
-      <div class="buttons">
+      <!-- <div class="buttons">
         <b-button type="is-primary" @click="submit">SUBMIT!!</b-button>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -46,34 +46,28 @@ export default {
   name: "Home",
   components: { VersionSelect, VueJsonPretty },
   data() {
-    return { updatedversions: {}, componentKey: 0 };
+    return { updatedversions: {}, componentKey: 0, tocopy: "" };
   },
   methods: {
-    versions(comp, lang) {
-      return json[comp][lang];
+    versions(comp) {
+      return json[comp];
     },
-    update(e, comp, lang, release) {
+    update(e, comp, modules, modulename) {
       if (!this.updatedversions[comp]) {
         this.updatedversions[comp] = {};
       }
-      if (!this.updatedversions[comp][lang]) {
-        this.updatedversions[comp][lang] = {};
+      if (!this.updatedversions[comp][modules]) {
+        this.updatedversions[comp][modules] = {};
       }
-      if (comp === "chaincode") {
-        if (lang === "node") {
-          this.updatedversions[comp][lang][release] = {
-            "fabric-shim": e.e.id,
-            "fabric-contract-api": e.e.id,
-          };
-        } else if (lang === "java") {
-          console.log();
-        }
-      } else if (comp === "sdk") {
-        console.log();
-      }
-
+      this.updatedversions[comp][modules][modulename] = e.e.id;
       this.componentKey += 1;
-    },
+
+      this.tocopy = `curl -H "Content-Type: application/json" --request POST  \\
+   -H "Accept: application/json" \\
+   -H "marvin: fb7f354c24a3974dc4432fb4ca6738f50db17d55a07c931f" \\
+   -d '${JSON.stringify(this.updatedversions)}' \\
+   https://devops-api.eu-gb.devops.cloud.ibm.com/v1/tekton-webhook/3648c0a9-a5da-478b-ab5b-d3712ec2eca6/run/94d17fdd-b50f-4a35-ae5f-5ea200f44811 `
+},
     async submit() {
       let TOKEN = "fb7f354c24a3974dc4432fb4ca6738f50db17d55a07c931f";
       let HOST =
@@ -82,8 +76,8 @@ export default {
         Accept: "application/json",
         "Content-Type": "application/json",
         marvin: TOKEN,
-        "Access-Control-Allow-Origin":"https://pages.github.ibm.com",
-        crossorigin:true
+        "Access-Control-Allow-Origin": "https://pages.github.ibm.com",
+        crossorigin: true,
       };
 
       var options = {
@@ -91,14 +85,13 @@ export default {
         method: "POST",
         headers: headers,
         data: {
-          versions:this.updatedversions,
+          versions: this.updatedversions,
         },
       };
 
-
       let result = await this.$http(options);
       if (result.status != 200) {
-        console.log(result.response.statusText)
+        console.log(result.response.statusText);
       } else {
         console.log(JSON.stringify(result.data));
       }
